5be8d35901dcd967b11c316e38409f17fb4f8fe4
Noah <noahtelussa@outlook.com>
2018-04-05 14:26:09 +0200

    commit

--- mazegame/src/mazegame/FieldTiles/Wall.java
+++ mazegame/src/mazegame/FieldTiles/Wall.java
@@ -33,14 +33,7 @@ public class Wall extends Field {
             g.drawRect(x, y, 30, 30);
     }
     
-    public static void getWall(){
-        for(Wall wall: Field.walls) {
-        System.out.println("Coord x : ");  
-        System.out.print(wall.coordX);  
-        System.out.println("Coord y : ");  
-        System.out.print(wall.coordY);  
-     }
-
+  
      /**
      * Return a arrayList with all the x and y of a wall
      *
